3046fe49d0df7d4327cd3dbaef903ce6f22f4dd3
Todd Sullivan <todd@sullivanltd.co.uk>
2020-03-06 15:22:24 +0000

    Corrected syntax error in array creation.

--- src/primelist/primelist.go
+++ src/primelist/primelist.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-// Only primes less than or equal to N will be generated
+// N  Only primes less than or equal to N will be generated
 const N = 1001
 
 func main() {
@@ -20,43 +20,46 @@ func main() {
 	}
 
 	N, err := strconv.Atoi(os.Args[1])
-	//N, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fmt.Println("Error occured: ", err)
+		return
+	}
 
 	nsqrt := math.Sqrt(float64(N))
 
-	is_prime := [N]bool{}
+	isPrime := make([]bool, N)
 
 	for x = 1; float64(x) <= nsqrt; x++ {
 		for y = 1; float64(y) <= nsqrt; y++ {
 			n = 4*(x*x) + y*y
 			if n <= N && (n%12 == 1 || n%12 == 5) {
-				is_prime[n] = !is_prime[n]
+				isPrime[n] = !isPrime[n]
 			}
 			n = 3*(x*x) + y*y
 			if n <= N && n%12 == 7 {
-				is_prime[n] = !is_prime[n]
+				isPrime[n] = !isPrime[n]
 			}
 			n = 3*(x*x) - y*y
 			if x > y && n <= N && n%12 == 11 {
-				is_prime[n] = !is_prime[n]
+				isPrime[n] = !isPrime[n]
 			}
 		}
 	}
 
 	for n = 5; float64(n) <= nsqrt; n++ {
-		if is_prime[n] {
+		if isPrime[n] {
 			for y = n * n; y < N; y += n * n {
-				is_prime[y] = false
+				isPrime[y] = false
 			}
 		}
 	}
 
-	is_prime[2] = true
-	is_prime[3] = true
+	isPrime[2] = true
+	isPrime[3] = true
 
 	primes := make([]int, 0, 1270606)
-	for x = 0; x < len(is_prime)-1; x++ {
-		if is_prime[x] {
+	for x = 0; x < len(isPrime)-1; x++ {
+		if isPrime[x] {
 			primes = append(primes, x)
 			fmt.Println(x)
 		}
